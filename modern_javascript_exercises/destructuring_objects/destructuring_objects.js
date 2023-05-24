42bd1c22d30634b37ac2e8cd07d7382ef52b7d75
kalyani <kalyani.nelluri54@gmail.com>
2023-05-24 18:01:10 +0100

    Implementation of  ternaries.js by using Ternary operator

--- modern_javascript_exercises/destructuring_objects/destructuring_objects.js
+++ modern_javascript_exercises/destructuring_objects/destructuring_objects.js
@@ -5,5 +5,11 @@ export function introduction(person) {
   const city = person.city;
   const pet = person.pet;
 
-  return "Hello my name is " + name + " and I live in " + city + " with " + pet;
+  // return "Hello my name is " + name + " and I live in " + city + " with " + pet;
+  const greetings = [];
+persons.forEach(person => {
+  const greeting = `Hello my name is, ${person.name} and I live in ${person.city} with${person.pet}.`;
+  greetings.push(greeting);
+});
+console.log(greetings);
 }
