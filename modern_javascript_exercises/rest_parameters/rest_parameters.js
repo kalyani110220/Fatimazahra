7d3fcabcb72e489cc3a67925c567c6460b6a35a9
kalyani <kalyani.nelluri54@gmail.com>
2023-05-31 14:41:40 +0100

    refactored rest-parameters

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,11 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
-}
+const add = (...numbers) => {
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+};
 
-add(1, 2, 3, 4, 5);
-
-module.exports = add;
+export default add;
