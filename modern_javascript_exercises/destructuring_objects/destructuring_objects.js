6a4621a95c6507c235daa57da000a22e5092c3c7
kalyani <kalyani.nelluri54@gmail.com>
2023-05-31 14:35:11 +0100

    Implemented destructuring-objects

--- modern_javascript_exercises/destructuring_objects/destructuring_objects.js
+++ modern_javascript_exercises/destructuring_objects/destructuring_objects.js
@@ -1,15 +1,15 @@
 // Instructions can be found in destructuring_objects.md
 
 export function introduction(person) {
-  const name = person.name;
-  const city = person.city;
-  const pet = person.pet;
-
-  // return "Hello my name is " + name + " and I live in " + city + " with " + pet;
-  const greetings = [];
-persons.forEach(person => {
-  const greeting = `Hello my name is, ${person.name} and I live in ${person.city} with${person.pet}.`;
-  greetings.push(greeting);
-});
-console.log(greetings);
+  const personInfo = Object.entries(person);
+  {
+    return (
+      "Hello my name is " +
+      person.name +
+      " and I live in " +
+      person.city +
+      " with " +
+      person.pet
+    );
+  }
 }
