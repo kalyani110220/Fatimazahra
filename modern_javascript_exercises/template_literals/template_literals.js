9848076e400598898653e7e6e9f7baedf0aa0a9e
kalyani <kalyani.nelluri54@gmail.com>
2023-05-31 14:50:17 +0100

    implemented template-literals

--- modern_javascript_exercises/template_literals/template_literals.js
+++ modern_javascript_exercises/template_literals/template_literals.js
@@ -1,9 +1,6 @@
 // Instructions can be found in template_literals.md
 
 export const greeting = (name, dayOfTheWeek) =>
-  // "Hello " + name + "! It is now " + dayOfTheWeek;
-  let name = Jimbob,
-  let dayOfTheWeek = Tuesday;
-  'Hello ${name},! It is now ${dayOfTheWeek}.';
+   `Hello ${name}! It is now ${dayOfTheWeek}`;
   
  
